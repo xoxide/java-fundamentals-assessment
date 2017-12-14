4431190e6f7da5f493132c2fcf53d82852bed26c
Cory Imel <cory.imel@gmail.com>
2017-12-14 14:56:47 -0600

    finished #5 (untested)

--- src/Assessment.java
+++ src/Assessment.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Assessment {
     public static int square (int number){
         return number * number;
@@ -22,9 +24,19 @@ public class Assessment {
     public static void main(String[] args) {
         int[] array = {1,2,3,4};
 
+
         System.out.println(square(5));
         System.out.println(sum(2,2));
         System.out.println(sum(2.3,2.2));
         System.out.println(average(array));
+
     }
+
+        public static ArrayList<User> capitalizeRecords(ArrayList<User> users){
+                for (User user : users){
+                    user.setFirstName(user.getFirstName().substring(0, 1).toUpperCase() + user.getFirstName().substring(1));
+                    user.setFirstName(user.getLastName().substring(0, 1).toUpperCase() + user.getLastName().substring(1));
+                }
+                return users;
+        }
 }
